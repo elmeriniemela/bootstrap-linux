f7d0f3efe14fa20a845f3412026419fa10b2d691
Elmeri Niemelä <niemela.elmeri@gmail.com>
2020-11-06 15:14:14 +0200

    Re-write tmux vscode

--- files/tmux-vscode.sh
+++ files/tmux-vscode.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-SESSION="vscode`pwd | sed s/[^a-zA-Z0-9_]/-/g`"
-tmux attach-session -d -t $SESSION || tmux new-session -s $SESSION
+# Referenced from vscode settings.json
+# "terminal.integrated.shell.linux": "$HOME/.config/bootstrap-linux/files/tmux-vscode.sh",
+tmux_session="vscode`pwd | sed s/[^a-zA-Z0-9_]/-/g`"
+# Add hash so that parent folder is also opended to own session even if subfolder sessions exist.
+# By default tmux attaches a session if substring matches, and we add a hash to break the match of parent folder to subfolder.
+tmux_session_hashed=$tmux_session-$((0x$(sha1sum <<<$tmux_session|cut -c1-2)))
+tmux attach-session -d -t $tmux_session_hashed || tmux new-session -s $tmux_session_hashed
