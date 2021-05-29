f631aa375a788d3046288796a6d046594d503b5b
Elmeri Niemelä <niemela.elmeri@gmail.com>
2021-05-29 15:19:25 +0300

    Rename install.py -> bootstrap.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name='bootstrap-linux',
-    py_modules=['install'],
+    py_modules=['bootstrap'],
     description='Wrapper to install default apps on Linux enviroments',
     license='GNU GPLv3',
     author='Elmeri Niemela',
     version='1.0',
     include_package_data=True,
     install_requires=[],
-    entry_points={'console_scripts': ['bootstrap-linux = install:main',],},
+    entry_points={'console_scripts': ['bootstrap-linux = bootstrap:main',],},
 )
