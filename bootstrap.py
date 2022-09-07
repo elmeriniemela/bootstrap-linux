1cbbf2ac8012673fe02dedf4e485dea9e9a991eb
Elmeri Niemelä <niemela.elmeri@gmail.com>
2022-09-07 18:15:11 +0300

    Fix ufw local ip rule

--- bootstrap.py
+++ bootstrap.py
@@ -645,7 +645,7 @@ def secure():
         # 'sudo ufw allow 80/tcp',
         # 'sudo ufw allow 443/tcp',
         # 'sudo ufw allow syncthing',
-        # 'sudo ufw allow from 192.168.1.0/24',
+        # 'sudo ufw allow from 192.168.1.0/16',
         'sudo ufw default deny incoming',
         'sudo ufw default allow outgoing',
         'sudo ufw enable',
