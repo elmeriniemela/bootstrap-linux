8c33e98984fbf2eb135f4207dbd2e4882fdce401
Elmeri Niemelä <niemela.elmeri@gmail.com>
2022-10-04 18:27:12 +0300

    Improve scripts for arcolinux

--- bootstrap_laptop.sh
+++ bootstrap_laptop.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-set -e # Fail early
-
 git clone https://github.com/elmeriniemela/bootstrap-linux.git ~/.config/bootstrap-linux
 cd ~/.config/bootstrap-linux
 sudo pacman -S python-pip
