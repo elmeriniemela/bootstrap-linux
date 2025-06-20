785de05440ea47dd042743c4038ad955ebb0b028
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-06-20 15:11:38 +0300

    separate lib

---
+++ lib.py
@@ -0,0 +1,203 @@
+import subprocess
+import os
+import sys
+from contextlib import contextmanager
+import distutils.spawn
+import re
+import random
+from functools import partial
+
+@contextmanager
+def _quittable():
+    try:
+        yield
+    except (EOFError, KeyboardInterrupt):
+        print("Bye")
+
+def _path(path):
+    if path.startswith('~/'):
+        formatted = os.path.join(os.path.expanduser('~'), path[2:])
+    else:
+        formatted = path
+    return formatted
+
+def _pipe(data, command):
+    default_kwargs = {
+        'shell': True,
+        'stdout': subprocess.PIPE,
+        'stderr': subprocess.PIPE,
+        'stdin': subprocess.PIPE,
+        'encoding': 'utf-8'
+    }
+    p = subprocess.Popen(
+        command,
+        **default_kwargs,
+    )
+    stdout_data, stderr_data = p.communicate(input=data)
+    if stderr_data:
+        print(stderr_data)
+    assert p.returncode == 0
+    return stdout_data
+
+def _enable(services, try_now=True):
+    _run([f'sudo systemctl enable {service}' for service in services])
+    if try_now:
+        try:
+            _run([f'sudo systemctl start {service}' for service in services])
+        except:
+            pass
+
+def _run(commands, dependencies=None, ignore_errors=False, **kwargs):
+    for command in commands:
+        print("Running command: ", command)
+        if command.startswith('cd'):
+            folder = command[3:]
+            os.chdir(_path(folder))
+        else:
+            default_kwargs = {
+                'shell': True,
+                'stdout': sys.stdout,
+                'stderr': sys.stderr,
+                'check': True,
+                'encoding': 'utf-8'
+            }
+            default_kwargs.update(kwargs)
+            try:
+                subprocess.run(command, **default_kwargs)
+            except subprocess.CalledProcessError as error:
+                if dependencies:
+                    dependencies()
+                    # Retry after running dependencies.
+                    _run([command], **kwargs)
+                elif ignore_errors:
+                    pass
+                else:
+                    raise
+
+def _installed_packages():
+    return set(subprocess.check_output(
+            "pacman -Qqe --groups | awk '{print $1}' && pacman -Qqe",
+            shell=True,
+            encoding='utf-8',
+        ).splitlines())
+
+def _packages(list_of_packages, flags=('-S', '--noconfirm', '--needed')):
+    prepend = ''
+    if os.geteuid() != 0:
+        prepend = 'sudo '
+
+    existing = _installed_packages()
+    list_of_packages = [p for p in list_of_packages if p not in existing]
+
+    if '-S' in flags and not list_of_packages:
+        return
+
+    flag_str = ' '.join(flags)
+    _run([
+        f'{prepend}pacman {flag_str} ' + ' '.join(list_of_packages)
+    ])
+
+def _yay(src=False):
+    if not os.path.exists('/usr/bin/yay'):
+        if src:
+            dst = '/tmp/yay'
+            _run([
+                f'git clone https://aur.archlinux.org/yay.git {dst}',
+                f'cd {dst}',
+                'makepkg -si',
+                f'rm -rf {dst}'
+            ])
+        else:
+            # https://aur.chaotic.cx/docs
+            _run([
+                'sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com',
+                'sudo pacman-key --lsign-key 3056513887B78AEB',
+                "sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'",
+                "sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'",
+            ])
+            _lineinfile({'/etc/pacman.conf': '[chaotic-aur]'})
+            _lineinfile({'/etc/pacman.conf': 'Include = /etc/pacman.d/chaotic-mirrorlist'})
+            _packages([], flags='-Syy'.split())
+            _packages(['yay'])
+
+
+def _aur(list_of_packages, flags=('-S', '--noconfirm', '--needed'), deps=False):
+    if os.geteuid() == 0:
+        print("Do not run this as root")
+        return
+    flag_str = ' '.join(flags)
+
+    existing = _installed_packages()
+    list_of_packages = [p for p in list_of_packages if p not in existing]
+
+    _run([
+        f'yay {flag_str} ' + ' '.join(list_of_packages)
+    ], dependencies=_yay if deps else False)
+
+def _lineinfile(files_dict):
+    prepend = ''
+    if os.geteuid() != 0:
+        prepend = 'sudo '
+
+    for filename, line in files_dict.items():
+        line = line.replace(r"'", r"\'")
+        _run([
+            f"{prepend}touch {filename}",
+            f"grep -qxF '{line}' {filename} || echo '{line}' | {prepend}tee -a {filename}",
+        ])
+
+def _link(files_dict):
+    prepend = ''
+    if os.geteuid() != 0:
+        prepend = 'sudo '
+    for fname, dest_path in files_dict.items():
+        if os.path.isfile(dest_path):
+            _run([f'{prepend}rm {dest_path}'])
+        _run([f'{prepend}ln {os.path.join(FILES_DIR, fname)} {dest_path}'], ignore_errors=True)
+
+def _copy(files_dict):
+    prepend = ''
+    if os.geteuid() != 0:
+        prepend = 'sudo '
+    for dest_path, fname in files_dict.items():
+        if os.path.isfile(dest_path):
+            _run([f'{prepend}rm {dest_path}'])
+        _run([f'{prepend}cp {os.path.join(FILES_DIR, fname)} {dest_path}'])
+
+class _Monitor():
+    def __init__(self, name, width=0, height=0, x=0, y=0, off=False):
+        self.name = name
+        self.width = int(width)
+        self.height = int(height)
+        self.x = int(x)
+        self.y = int(y)
+        self.off = off
+        self.primary = False
+
+    def __eq__(self, other):
+        return self.width == other.width
+
+    def __ne__(self, other):
+        return self.width != other.width
+
+    def __gt__(self, other):
+        return self.width > other.width
+
+    def __ge__(self, other):
+        return self.width >= other.width
+
+    def __lt__(self, other):
+        return self.width < other.width
+
+    def __le__(self, other):
+        return self.width <= other.width
+
+
+    def __str__(self):
+        if self.off:
+            return f'--output {self.name} --off'
+        prim_flag = ' --primary' if self.primary else ''
+        return f'--output {self.name}{prim_flag} --mode {self.width}x{self.height} --pos {self.x}x{self.y}'
+
+    def __repr__(self):
+        return f'_Monitor(name={self.name!r}, width={self.width!r}, height={self.height!r}, x={self.x!r}, y={self.y!r}, off={self.off!r})'
