15b63871d4715c0f28fd48e03d205037f1bc401a
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-06-20 13:42:31 +0300

    fix fatal error

--- bootstrap_laptop.sh
+++ bootstrap_laptop.sh
@@ -3,10 +3,8 @@
 set -e # Fail early
 sudo pacman -S python-pip git python-distutils-extra
 
-export BOOTSTRAP_TARGET_DIR="~/.config/bootstrap-linux"
-git -C "$BOOTSTRAP_TARGET_DIR" pull || git clone https://github.com/<repo>.git "$BOOTSTRAP_TARGET_DIR"
-git clone https://github.com/elmeriniemela/bootstrap-linux.git
-cd "$BOOTSTRAP_TARGET_DIR"
+git -C ~/.config/bootstrap-linux pull || git clone https://github.com/elmeriniemela/bootstrap-linux.git ~/.config/bootstrap-linux
+cd ~/.config/bootstrap-linux
 sudo pip install --break-system-packages --editable .
 
 python bootstrap.py archinstall
