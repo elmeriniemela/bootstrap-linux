a6d87e271ec459a4ea2fd76176d738478cfb638d
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-07-10 12:47:08 +0300

    update bootstrap linux

--- bootstrap_laptop.sh
+++ bootstrap_laptop.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
 
 set -e # Fail early
-sudo pacman -S python-pip git python-distutils-extra
+sudo pacman -S python-pip git python-distutils-extra --needed
 
-git clone https://github.com/elmeriniemela/bootstrap-linux.git ~/.config/bootstrap-linux || :
+git clone https://github.com/elmeriniemela/bootstrap-linux.git ~/.config/bootstrap-linux || cd ~/.config/bootstrap-linux && git pull:
 cd ~/.config/bootstrap-linux
-git pull
 sudo pip install --break-system-packages --editable .
 
 python bootstrap.py distro
