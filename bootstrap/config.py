463de91935a8cd928b0b2105ab0a1a54089a4b68
Elmeri Niemelä <niemela.elmeri@gmail.com>
2026-08-20 20:54:52 +0300

    add rule

--- bootstrap/config.py
+++ bootstrap/config.py
@@ -81,7 +81,7 @@ def secure():
         # 'sudo ufw allow 80/tcp',
         # 'sudo ufw allow 443/tcp',
         # 'sudo ufw allow syncthing',
-        # 'sudo ufw allow from 192.168.1.0/16',
+        'sudo ufw allow from 192.168.1.0/24 to any port 22 proto tcp',
         'sudo ufw default deny incoming',
         'sudo ufw default allow outgoing',
         'sudo ufw enable',
